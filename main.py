abd5d2ffa266140ee136825a09958d80f4ec8f65
Anton Tyum <anton.1011@mail.ru>
2024-02-22 20:59:42 +0500

    correction

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import json
 from datetime import datetime
 from pprint import pprint
+
+import requests
+
 import vkapi as vk
 import yandapi as ya
-import os
 
 def load_cfg(filename='config.json'):
     with open(filename) as f:
@@ -22,29 +24,34 @@ while not user_id.isdigit():
     print("Ошибка! Введите числовой идентификатор пользователя.")
     user_id = input('Введите ID пользователя ВК :').strip()
 
+class VKAPIClient:
+    ...
+    def get_proile_photos_info(self):
+        params = {
+            'access_token': self.token,
+            'v': self.api_version,
+            'owner_id': self.user_id,
+            'album_id': 'profile',
+            'extended': '1'
+        }
+        response = requests.get(self.base_url + 'photos.get', params=params)
+        return response.json()
+    ...
 
 vk_client = vk.VKAPIClient(vk_token, user_id)
 ya_client = ya.YandexDiskAPIClient(ya_token)
-photos_info = []
+photos_info = {}
 
 def get_photo_url_n_likes(quantity=5):
-
     photo_info = vk_client.get_proile_photos_info()
-    photos = photo_info.get('response', {})['items']
-    photo_tuples = []
-    likes_pool = []
-    n = 0
-    if len(photos) < quantity: quantity = len(photos)
-    while n < quantity:
-        photo_tuples.append((photos[n].get('sizes', [])[-1]['url'],
-                             vk_client.get_likes_count(photos[n].get('id', 0)),
-                             photos[n].get('date', 0),
-                             photos[n].get('sizes', [])[-1]['type']))
-        likes_pool.append(vk_client.get_likes_count(photos[n].get('id', 0)))
-        n += 1
-    uniq_likes_pool = set([i for i in likes_pool if likes_pool.count(i) > 1])
-    return photo_tuples, uniq_likes_pool
-
+    photos = photo_info.get('response', {}).get('items', [])
+    for photo in photos:
+        photo_url = photo['sizes'][-1]['url']
+        likes = photo.get('likes', {}).get('count', 0)
+        date = photo['date']
+        photo_type = photo['sizes'][-1]['type']
+        photos_info[likes] = {'url': photo_url, 'date': date, 'type': photo_type}
+    return photos_info
 
 def backup_photos(vk_id, quantity=5):
     def progress_bar(n, width=50, file=''):
@@ -53,37 +60,25 @@ def backup_photos(vk_id, quantity=5):
         bar = '*' * filled + '-' * (width - filled)
         print(f'\r[{bar}] {percent * 100:.2f}% {file}', end='')
 
-    def fill_json(filename, size):
-        return {'filename': filename, 'size': size}
+    def upload_photo(filename, data):
+        ya_client.upload_file_by_url(data['url'], f'{filename}.jpg')
 
-    def upload_photo(filename, type):
-        ya_client.upload_file_by_url(url, f'{filename}.jpg')
-        photos_info.append(fill_json(f'{filename}.jpg', type))
-
-    def upload_json(filename, var):
-        ya_client.upload_var(f'backup_{vk_id}/{filename}.json', json.dumps(var))
-
-    url_and_likes, likes_pool = get_photo_url_n_likes(quantity)
+    url_and_likes = get_photo_url_n_likes(quantity)
 
     n = 0
     progress_bar(n)
-    for url, likes, date, type in url_and_likes:
-        if likes in likes_pool:
-            date = datetime.fromtimestamp(date).strftime('%Y-%m-%d_%H-%M-%S')
-            # print(date)
-            filename = f'backup_{vk_id}/{likes}-{date}'
-            upload_photo(filename, type)
-        else:
-            filename = f'backup_{vk_id}/{likes}'
-            upload_photo(filename, type)
+    for likes, data in url_and_likes.items():
+        date = datetime.fromtimestamp(data['date']).strftime('%Y-%m-%d_%H-%M-%S')
+        filename = f'backup_{vk_id}/{likes}-{date}'
+        upload_photo(filename, data)
         progress_bar(n, file=f'{filename}.jpg')
         n += 1
-
 
     progress_bar(quantity)
     print()
     pprint(photos_info)
-    upload_json(f'backup_{vk_id}', photos_info)
+    with open(f'backup_{vk_id}.json', 'w') as json_file:
+        json.dump(photos_info, json_file)
 
 def user_input():
     while True:
@@ -100,6 +95,3 @@ def user_input():
     backup_photos(user_id, quantity=quantity)
 
 user_input()
-
-os.system('pip freeze > requirements.txt')
-print("Список всех зависимостей успешно добавлен в requirements.txt.")
