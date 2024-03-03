59a60c1f0a4079d522f7703a14268742a757cdd4
Anton Tyum <anton.1011@mail.ru>
2024-03-03 10:00:38 +0500

    changes_03\03

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import json
 from datetime import datetime
-from pprint import pprint
-
 import requests
 
-import vkapi as vk
 import yandapi as ya
 
 def load_cfg(filename='config.json'):
@@ -12,73 +9,76 @@ def load_cfg(filename='config.json'):
         cfg = json.load(f)
     return cfg
 
-vk_token = input("Введите токен ВК (Нажмите Enter если токен добавлен в config.json): ")
-if vk_token == "":
-    vk_token = load_cfg()['vk_token']
-ya_token = input("Введите токен Яндекс (Нажмите Enter если токен добавлен в config.json): ")
-if ya_token == '':
-    ya_token = load_cfg()['ya_token']
+def backup_photos(vk_id, vk_token, ya_token, quantity=5):
+    ya_client = ya.YandexDiskAPIClient(ya_token)
 
-user_id = input('Введите ID пользователя ВК :').strip()
-while not user_id.isdigit():
-    print("Ошибка! Введите числовой идентификатор пользователя.")
-    user_id = input('Введите ID пользователя ВК :').strip()
-
-class VKAPIClient:
-    def get_proile_photos_info(self):
-        params = {
-            'access_token': self.token,
-            'v': self.api_version,
-            'owner_id': self.user_id,
-            'album_id': 'profile',
-            'extended': '1'
-        }
-        response = requests.get(self.base_url + 'photos.get', params=params)
-        return response.json()
-
-vk_client = vk.VKAPIClient(vk_token, user_id)
-ya_client = ya.YandexDiskAPIClient(ya_token)
-photos_info = {}
-
-def get_photo_url_n_likes(quantity=5):
-    photo_info = vk_client.get_proile_photos_info()
-    photos = photo_info.get('response', {}).get('items', [])
-    for photo in photos:
-        photo_url = photo['sizes'][-1]['url']
-        likes = photo.get('likes', {}).get('count', 0)
-        date = photo['date']
-        photo_type = photo['sizes'][-1]['type']
-        photos_info[likes] = {'url': photo_url, 'date': date, 'type': photo_type}
-    return photos_info
-
-def backup_photos(vk_id, quantity=5):
     def progress_bar(n, width=50, file=''):
         percent = n / quantity
         filled = int(width * percent)
         bar = '*' * filled + '-' * (width - filled)
         print(f'\r[{bar}] {percent * 100:.2f}% {file}', end='')
 
+    def get_profile_photos_info(token, user_id):
+        base_url = "https://api.vk.com/method/"
+        api_version = "5.131"
+        params = {
+            'access_token': token,
+            'v': api_version,
+            'owner_id': user_id,
+            'album_id': 'profile',
+            'extended': '1'
+        }
+        response = requests.get(base_url + 'photos.get', params=params)
+        return response.json()
+
     def upload_photo(filename, data):
         ya_client.upload_file_by_url(data['url'], f'{filename}.jpg')
 
-    url_and_likes = get_photo_url_n_likes(quantity)
-
     n = 0
-    progress_bar(n)
-    for likes, data in url_and_likes.items():
-        date = datetime.fromtimestamp(data['date']).strftime('%Y-%m-%d_%H-%M-%S')
-        filename = f'backup_{vk_id}/{likes}-{date}'
-        upload_photo(filename, data)
-        progress_bar(n, file=f'{filename}.jpg')
-        n += 1
+    photos_info = {}
+
+    photo_info = get_profile_photos_info(vk_token, vk_id)
+    photos = photo_info.get('response', {}).get('items', [])
+
+    for photo in photos:
+        photo_url = photo['sizes'][-1]['url']
+        likes = photo.get('likes', {}).get('count', 0)
+        photo_type = photo['sizes'][-1]['type']
+
+        if likes in photos_info:
+            if 'date' not in photos_info[likes]:
+                photos_info[likes]['date'] = photo['date']
+        else:
+            photos_info[likes] = {'url': photo_url, 'type': photo_type}
+            if 'date' not in photos_info[likes]:
+                photos_info[likes]['date'] = photo['date']
+
+        if likes in photos_info and len(photos_info) <= quantity:
+            date_str = datetime.fromtimestamp(photos_info[likes]['date']).strftime('%Y-%m-%d_%H-%M-%S')
+            filename = f'backup_{vk_id}/{likes}-{date_str}.jpg'
+            upload_photo(filename, photos_info[likes])
+            progress_bar(n, file=filename)
+            n += 1
 
     progress_bar(quantity)
     print()
-    pprint(photos_info)
+
     with open(f'backup_{vk_id}.json', 'w') as json_file:
-        json.dump(photos_info, json_file)
+        json.dump(photos_info, json_file, indent=2)
 
 def user_input():
+    vk_token = input("Введите токен ВК (Нажмите Enter если токен добавлен в config.json): ")
+    if vk_token == "":
+        vk_token = load_cfg()['vk_token']
+    ya_token = input("Введите токен Яндекс (Нажмите Enter если токен добавлен в config.json): ")
+    if ya_token == '':
+        ya_token = load_cfg()['ya_token']
+
+    user_id = input('Введите ID пользователя ВК: ').strip()
+    while not user_id.isdigit():
+        print("Ошибка! Введите числовой идентификатор пользователя.")
+        user_id = input('Введите ID пользователя ВК: ').strip()
+
     while True:
         quantity_input = input("Введите количество фотографий для загрузки: ")
         if quantity_input.strip():
@@ -90,6 +90,6 @@ def user_input():
         else:
             print("Ошибка! Введите количество фотографий.")
 
-    backup_photos(user_id, quantity=quantity)
+    backup_photos(user_id, vk_token, ya_token, quantity=quantity)
 
 user_input()
